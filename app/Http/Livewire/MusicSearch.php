7eb8d601242e08363a742bab1916a7953fb2f4e6
abdallah <abdallahelmekawy6@gmail.com>
2023-03-01 18:48:36 +0200

    music search component setup

---
+++ app/Http/Livewire/MusicSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MusicSearch extends Component
+{
+    public function render()
+    {
+        return view('livewire.music-search');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/contact', function () {
     return view('contact-us');
 });
+Route::get('/music', function () {
+    return view('music');
+});
